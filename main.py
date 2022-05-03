3bdaaa207dc25c960bd8b03d2410414ad055423f
ShivamKantPrasad <shivamkantprasad@gmail.com>
2022-05-03 19:38:43 +0530

    added collision to car and can complete laps in the map

--- main.py
+++ main.py
@@ -39,15 +39,40 @@ def handle_events():
 		keys['brake'] = True
 	return keys
 
+def lineLine(x1, y1, x2, y2, x3, y3, x4, y4):
+	dnom = ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
+	if dnom == 0:
+		return False
+	uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / dnom
+	uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / dnom
+	if (uA >= 0 and uA <= 1 and uB >= 0 and uB <= 1):
+		# intersectionX = x1 + (uA * (x2 - x1))
+		# intersectionY = y1 + (uA * (y2 - y1))
+		# pygame.draw.circle(screen, (255, 0, 0), (int(intersectionX), int(intersectionY)), 5)
+		return True
+
+def lineRect(P1, P2, FL, FR, BR, BL):
+	left = lineLine(*P1, *P2 , *FL, *BL)
+	right = lineLine(*P1, *P2 ,*FR,*BR)
+	front = lineLine(*P1, *P2 ,*FL,*FR)
+	back = lineLine(*P1, *P2 ,*BL,*BR)
+	if left or right or front or back:
+		return True
+	return False
+
 class map:
 	def __init__(self):
 		self.loaded_map = 0
 		self.list_of_turns = []
 		self.list_of_lines = self.calc_lines(self.list_of_turns, 80)
 		self.wall1, self.wall2 = self.colide_lines(*self.list_of_lines)
+		self.checkpoints = []
 		self.load_next_map(zoom)
 
-
+	def get_checkpoints(self):
+		self.checkpoints = []
+		for x in range(len(self.wall1)):
+			self.checkpoints.append((self.wall1[x],self.wall2[x]))
 
 	def load_next_map(self, zoom):
 		width = 80*zoom
@@ -70,8 +95,10 @@ class map:
 		if self.list_of_turns:
 			self.list_of_lines = self.calc_lines(self.list_of_turns, width)
 			self.wall1, self.wall2 = self.colide_lines(*self.list_of_lines)
-			self.startangle = (vector2(self.list_of_turns[1]) - vector2(self.list_of_turns[0])).normalize().as_polar()[1]
-			self.startpos = vector2(self.list_of_turns[0])
+			self.startangle = (vector2(self.list_of_turns[2]) - vector2(self.list_of_turns[1])).normalize().as_polar()[1]
+			self.startpos = vector2(self.list_of_turns[1])
+
+		self.get_checkpoints()
 
 	def calc_lines(self,list_of_points, road_width):
 		lines1 = []
@@ -130,10 +157,11 @@ class map:
 			point_out.append(vector2(point_x, point_y))
 		return point_in, point_out
 
-	def draw(self,wall1, wall2, screen):
+	def draw(self,wall1, wall2, screen, checkpoint = []):
 		if len(wall1) >= 2 and len(wall2) >= 2:
 			pygame.draw.polygon(screen, (0,0,0), wall1, 3)
 			pygame.draw.polygon(screen, (0,0,0), wall2, 3)
+			pygame.draw.line(screen, (255,0,0), wall1[0], wall2[0], 3)
 
 	def load_from_json(self,file_num):
 		print("Loading map" + str(file_num))
@@ -166,10 +194,23 @@ class car:
 		self.carbody = pygame.image.load('carbody.png')
 		self.carbody = pygame.transform.scale(self.carbody, (int(self.size.x), int(self.size.y)))
 		self.acceleration = 0.0
+		self.corners = {'FL': None, 'FR': None, 'BR': None, 'BL': None}
+		self.checkpoints_got = set([0])
+		self.laps = 0
+		self.update_corners()
+
+	def update_corners(self):
+		self.corners = {'FL': None, 'FR': None, 'BR': None, 'BL': None, 'order': ['FL','FR','BR','BL']}
+		self.corners['FL'] = (self.pos + pygame.Vector2(self.size.x / 2, -self.size.y / 2).rotate(-self.angle))
+		self.corners['FR'] = (self.pos + pygame.Vector2(self.size.x / 2, self.size.y / 2).rotate(-self.angle))
+		self.corners['BR'] = (self.pos + pygame.Vector2(-self.size.x / 2, self.size.y / 2).rotate(-self.angle))
+		self.corners['BL'] = (self.pos + pygame.Vector2(-self.size.x / 2, -self.size.y / 2).rotate(-self.angle))
 	
 	def draw(self, screen):
 		tempsurf = pygame.transform.rotate(self.carbody, self.angle)
 		screen.blit(tempsurf, self.pos - pygame.Vector2(tempsurf.get_rect().size) / 2)
+		for x in self.corners['order']:
+			pygame.draw.circle(screen, (255,0,255), self.corners[x], 3)
 	
 	def update(self, keys):
 		self.acceleration = 0.0
@@ -208,20 +249,45 @@ class car:
 			self.dir = self.dir.lerp(dir.normalize(), 0.9)
 		self.pos = (front_wheel + back_wheel) / 2
 		self.angle = -dir.as_polar()[1]
-		
-		# pygame.draw.circle(screen, (255, 0, 0), self.pos, 5)
-		# pygame.draw.circle(screen, (0, 255, 0), front_wheel, 5)
-		# pygame.draw.circle(screen, (0, 0, 255), back_wheel, 5)
-	
-	def rotateAnim(self):
-		self.angle += 1
-		pygame.draw.circle(screen, (255, 0, 0), self.pos, 5)
-	
+
+		self.update_corners()
+
 	def calcwheel(self):
 		dir = self.dir
 		front_wheel = self.pos + dir * self.size.x * 0.4
 		back_wheel = self.pos - dir * self.size.x * 0.4
 		return front_wheel, back_wheel
+
+	def collide(self, wall1, wall2, screen):
+		for x in range(len(wall1)):
+			if wall1[x-1] != wall1[x]:
+				if lineRect(wall1[x-1], wall1[x], self.corners['FL'],self.corners['FR'],self.corners['BR'],self.corners['BL']):
+					if self.speed > 0:
+						self.speed = -3
+					else:
+						self.speed = 3
+					return
+		for x in range(len(wall2)):
+			if wall2[x - 1] != wall2[x]:
+				if lineRect(wall2[x-1], wall2[x], self.corners['FL'],self.corners['FR'],self.corners['BR'],self.corners['BL']):
+					if self.speed > 0:
+						self.speed = -3
+					else:
+						self.speed = 3
+					return
+
+
+	def collide_checkpoint(self,screen,checkpoints):
+
+		for x in range(len(checkpoints)):
+
+			if lineRect(checkpoints[x][0],checkpoints[x][1],self.corners['FL'],self.corners['FR'],self.corners['BR'],self.corners['BL']):
+				if (x-1)%len(checkpoints) in self.checkpoints_got:
+					if x == 0:
+						self.laps += 1
+						self.checkpoints_got = {0}
+						print('lap done')
+					self.checkpoints_got.add(x)
 
 
 Map = map()
@@ -245,4 +311,6 @@ while True:
 	# print(keys)
 	mycar.draw(screen)
 	mycar.update(keys)
+	mycar.collide_checkpoint(screen, Map.checkpoints)
+	mycar.collide(Map.wall1,Map.wall2,screen)
 	pygame.display.flip()
